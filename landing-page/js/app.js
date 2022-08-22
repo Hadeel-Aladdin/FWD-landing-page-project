c126e0c4f1beb30bd7dfa45855259cfe1a0747a1
Hadeel Alaa <hadeel_20180676@fci.helwan.edu.eg>
2022-08-22 03:30:19 +0200

    Build dynamic Navigation bar and add the fourth section in the page

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,62 +1,21 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+/* jshint esversion: 6*/
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+// Global Variables:
+const fragement = document.createDocumentFragment();
+const sections = [...document.querySelectorAll('section')];
+const nav = document.querySelector('#navbar__list');
 
-/**
- * Define Global Variables
- * 
-*/
+// Functions
+function addNavItems(){
+    sections.map(function (section){
+        const secName = section.getAttribute('data-nav');
+        const secId = section.getAttribute('id');
+        const li = document.createElement('li');
+        //li.classList.add("nav__item");
+        li.innerHTML = `<a class = 'menu__link' href = '#${secId}'> ${secName} </a>`;
+        fragement.appendChild(li);
+    });
+    nav.appendChild(fragement);
+}
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+addNavItems();
